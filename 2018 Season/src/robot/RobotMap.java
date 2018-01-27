0300ca901fa5779ec21baaac5547307a2d7044df
R3P2 <riverdaleraidersrobotics@gmail.com>
2018-01-27 11:39:13 -0500

    Initial Commit
    Added files from previous project folder

---
+++ 2018 Season/src/robot/RobotMap.java
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+	public final static int LEFT_MOTOR_PORT = 0;
+	public final static int RIGHT_MOTOR_PORT = 0;
+	
+	public final static int LEFT_ENCODER_PORT_ONE = 0;
+	public final static int LEFT_ENCODER_PORT_TWO = 1;
+	public final static int RIGHT_ENCODER_PORT_ONE = 2;
+	public final static int RIGHT_ENCODER_PORT_TWO = 3;
+	
+	public final static double KP = 0.1;
+	
+	public static int LEFT_ENCODER_MAX_SPEED = 1800;
+	public static int RIGHT_ENCODER_MAX_SPEED = 2000;
+	
+}
---
+++ 2018 Season/src/robot/subsystems/ChassisSubsystem.java
@@ -0,0 +1,102 @@
+package robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import robot.RobotMap;
+import robot.commands.JoystickCommand;
+
+/**
+ *
+ */
+public class ChassisSubsystem extends Subsystem {
+
+	DoubleSolenoid PistonOne = new DoubleSolenoid(0, 1);
+	DoubleSolenoid PistonTwo = new DoubleSolenoid(2, 3);
+
+	// Our talon speed controlers. Only uncomment when talons are connected:
+	// TalonSRX leftMotor = new TalonSRX(1);
+	// TalonSRX rightMotor = new TalonSRX(0);
+
+	Victor leftMotor = new Victor(0);
+	Victor rightMotor = new Victor(1);
+
+	Encoder leftEncoder = new Encoder(0, 1);
+	Encoder rightEncoder = new Encoder(2, 3);
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		setDefaultCommand(new JoystickCommand());
+		leftMotor.setInverted(true);
+	}
+
+	public void setPistons(boolean state) {
+		PistonOne.set(state ? Value.kForward : Value.kReverse);
+		PistonTwo.set(state ? Value.kForward : Value.kReverse);
+	}
+
+	// These are the movement related methods
+	// Use these to move the robot
+	
+	public void setMotors(double rightSpeed, double leftSpeed) {
+		leftMotor.set(getPID(leftSpeed, leftEncoder.getDistance(), RobotMap.LEFT_ENCODER_MAX_SPEED));
+		rightMotor.set(getPID(leftSpeed, rightEncoder.getDistance(), RobotMap.RIGHT_ENCODER_MAX_SPEED));
+	}
+
+	public void setSpeed(double speed) {
+		setMotors(speed, speed);
+	}
+
+	public void setMovement(double speed, double turn) {
+		if (Math.abs(turn) < 0.2 && Math.abs(speed) > 0.1) {
+
+			setMotors(speed, speed);
+
+			System.out.println("running");
+		} else if (Math.abs(turn) > 0.2 && Math.abs(speed) > 0.1) {
+
+			if (turn > 0.2) {
+				setMotors(speed, (1 - turn) * speed);
+			} else {
+				setMotors((1 - Math.abs(turn)) * speed, speed);
+			}
+
+		} else if (Math.abs(turn) > 0.2) {
+
+			setMotors(-turn, turn);
+
+		} else {
+			setSpeed(0);
+		}
+	}
+
+	public double getPID(double speed, double feedback, int maxSpeed) {
+
+		double normalizedFeedback = feedback / maxSpeed;
+
+		if (normalizedFeedback > 1.0) {
+			normalizedFeedback = 1.0;
+		}
+		if (normalizedFeedback < -1.0) {
+			normalizedFeedback = -1.0;
+		}
+
+		double error = speed - normalizedFeedback;
+
+		double output = RobotMap.KP * error;
+
+		double normalizedOutput = output + speed;
+
+		if (normalizedOutput > 1.0) {
+			normalizedOutput = 1.0;
+		}
+		if (normalizedOutput < -1.0) {
+			normalizedOutput = -1.0;
+		}
+
+		return normalizedOutput;
+	}
+
+}
