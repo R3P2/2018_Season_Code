07a9c95de474315a9fd19281ed93f87778a314e4
GuyBlumenthal <rhindet@gmail.com>
2018-03-21 17:48:52 -0400

    I Literally Don't Know Why So Many Files Were Updated

--- 2018 Season/src/robot/subsystems/ChassisSubsystem.java
+++ 2018 Season/src/robot/subsystems/ChassisSubsystem.java
@@ -306,7 +306,7 @@ public class ChassisSubsystem extends Subsystem {
 	}
 
 	public void updateSmartDashboard() {
-		SmartDashboard.putData("Gyro", gyro);
+		SmartDashboard.putNumber("Gyro", gyro.getAngle());
 		SmartDashboard.putNumber("Left Encoder", getLeftEncoderCounts());
 		SmartDashboard.putNumber("Right Encoder", getRightEncoderCounts());
 		SmartDashboard.putNumber("Left Encoder Rate", getLeftEncoderRate());
